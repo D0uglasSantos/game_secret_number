087447fd806232fe19543ab28d0be0e29b6ba269
D0uglasSantos <douglas.aquino@citechnology.com.br>
2023-05-02 21:05:26 -0300

    last commit day 02/05

---
+++ app/validacao.js
@@ -0,0 +1,46 @@
+function verificarChuteValorValido(chute) {
+  const numero = +chute;
+
+  if (chuteInvalido(numero)) {
+    elementoChute.innerHTML += "<div> Valor Inválido! </div>";
+    return;
+  }
+
+  if (numeroMaiorOuMenorQuePermitido(numero)) {
+    elementoChute.innerHTML += `
+        <div> Valor inválido: Fale um número entre ${menorValor} e ${maiorValor} </div>
+        `;
+    return;
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+        <h2> Você Acertou!</h2>
+        <h3> O número secreto era ${numeroSecreto}</h3>
+        
+        <button id="jogar-novamente" class="btn-jogar-novamente">Jogar Novamente!</button>
+    `;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `
+    <div>O número secreto é menor <i class="fas fa-arrow-down"></i></i></div>
+    `;
+  } else {
+     elementoChute.innerHTML += `
+     <div>O número secreto é maior <i class="fas fa-arrow-up"></i></i></div>
+     `
+  }
+}
+
+function chuteInvalido(numero) {
+  return Number.isNaN(numero);
+}
+
+function numeroMaiorOuMenorQuePermitido(numero) {
+  return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
